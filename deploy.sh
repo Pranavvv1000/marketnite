50cee7301d86fcd760ab48eaac8b419a9e6a2297
Pranavvv1000 <132485339+Pranavvv1000@users.noreply.github.com>
2023-05-05 10:13:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m pip install --upgrade pip
-python -m pip3 install flask yfinance pandas numpy Figure
+pip3 install flask yfinance pandas numpy Figure
 pip3 install matplotlib
 
 
